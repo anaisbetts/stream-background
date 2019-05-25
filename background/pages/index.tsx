bd7380a9123a37e13b5c9329e135ceb61b824bd4
Anaïs Betts <anais@anaisbetts.org>
2019-05-24 21:31:40 -0700

    Wire it all up

--- background/pages/index.tsx
+++ background/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import Head from 'next/head';
+
 const SIDEBAR_WIDTH = 320;
 const FOOTER_HEIGHT = 64;
 const BROADCAST_WIDTH = 1920;
@@ -9,6 +11,10 @@ export default () => {
   const theWidth = ('window' in global) ? window.outerWidth : 0;
   return (
     <>
+      <Head>
+        <link href='https://fonts.googleapis.com/css?family=Pacifico&display=swap' rel='stylesheet'></link>
+      </Head>
+
       <div className='container'>
         <main>
           <h1>Main - {theWidth}</h1>
@@ -20,15 +26,18 @@ export default () => {
         </aside>
 
         <footer>
-          <h1>Footer</h1>
+          <div style={{marginLeft: 24}} />
+          <img src='/static/twitch.svg' style={{marginTop: 4.5}} />
+          <img src='/static/twitter.svg' style={{marginTop: 4.5}} />
+          <img src='/static/github.svg' style={{marginTop: 4.5}} />
+          <h2 style={{marginLeft: 8}}>anaisbetts</h2>
         </footer>
       </div>
 
       <style jsx global>{`
-      body,html,main,aside,footer,h1 {
+      body,html,main,aside,footer,h1,h2 {
         margin: 0;
         padding: 0;
-        overflow: hidden;
       }
 
       aside {
@@ -39,7 +48,14 @@ export default () => {
         grid-area: footer;
 
         display: flex;
+        flex-direction: row;
         align-items: center;
+        font-family: 'Pacifico';
+      }
+
+      footer img {
+        margin: 0px 8px 0 8px;
+        width: 32px;
       }
 
       main {
@@ -51,8 +67,8 @@ export default () => {
         max-width: ${BROADCAST_WIDTH}px;
         max-height: ${BROADCAST_HEIGHT}px;
         height: ${BROADCAST_HEIGHT}px;
-        background: blue;
-        color: white;
+        background: #eee;
+        color: black;
 
         display: grid;
         grid-template-columns: auto ${SIDEBAR_WIDTH}px;

13f0ca01504e507b9a8587fb6a7cefd470cfe210
Anaïs Betts <anais@anaisbetts.org>
2019-05-27 21:47:03 -0700

    Rename robot to API since we can't actually run it in now

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from 'http';
 
 export default (_req: IncomingMessage, res: ServerResponse) => {
   res.end('Hello world');
-}
+};

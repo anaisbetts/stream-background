8afb7f90f4546232d92670bc43b32b265dc239bb
Anaïs Betts <anais@anaisbetts.org>
2019-10-20 18:26:51 -0700

    Get alias doing....anything

--- alias/src/index.ts
+++ alias/src/index.ts
@@ -4,7 +4,8 @@ import { db } from './firebase';
 
 export default async function (_req: NowRequest, res: NowResponse) {
   const msgs = await db.collection('messages').limit(5).get();
+  console.log(msgs);
 
-  res.status(200).send(JSON.stringify(msgs));
+  res.status(200).send(JSON.stringify(msgs.docs.map(x => x.data().message)));
   res.end();
 }

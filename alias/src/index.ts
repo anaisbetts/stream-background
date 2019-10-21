a14ee1acb53b7f0c3ac57339a72e98f2d9b993b2
Anaïs Betts <anais@anaisbetts.org>
2019-10-20 19:22:50 -0700

    Finish the crappy version of the redirector

--- alias/src/index.ts
+++ alias/src/index.ts
@@ -2,10 +2,19 @@ import { NowRequest, NowResponse } from '@now/node';
 
 import { db } from './firebase';
 
-export default async function (_req: NowRequest, res: NowResponse) {
-  const msgs = await db.collection('messages').limit(5).get();
-  console.log(msgs);
+export default async function (req: NowRequest, res: NowResponse) {
+  const q: string = req.query;
 
-  res.status(200).send(JSON.stringify(msgs.docs.map(x => x.data().message)));
+  const slug = q['slug'].split('/')[0] || 'me';
+
+  const slugDoc = await db.collection('aliases').where('slug', '==', slug).limit(1).get();
+  if (slugDoc.docs.length < 1) {
+    res.status(404);
+    res.send('dunno lol');
+    return;
+  }
+
+  res.status(303);
+  res.setHeader('Location', slugDoc.docs[0].data().target);
   res.end();
 }

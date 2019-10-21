b847a1d70df3d30e89c4a31d562bbf3d17b26647
Anaïs Betts <anais@anaisbetts.org>
2019-10-20 19:44:08 -0700

    Add a script to add aliases

--- alias/src/index.ts
+++ alias/src/index.ts
@@ -3,9 +3,9 @@ import { NowRequest, NowResponse } from '@now/node';
 import { db } from './firebase';
 
 export default async function (req: NowRequest, res: NowResponse) {
-  const q: string = req.query;
+  const q: string = req.query['slug'];
 
-  const slug = q['slug'].split('/')[0] || 'me';
+  const slug = q.split('/')[0] || 'me';
 
   const slugDoc = await db.collection('aliases').where('slug', '==', slug).limit(1).get();
   if (slugDoc.docs.length < 1) {

69f3b57ea83b49334f933e14d998f02c6a0576ec
Anaïs Betts <anais@anaisbetts.org>
2019-10-28 15:49:49 -0700

    Add example config file

---
+++ bot/src/firebase-config.example.ts
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+export const firebaseBotUser = '';
+export const firebaseBotPassword = '';
+export const defaultTwitchUsername = '';
+export const defaultTwitchChannel = defaultTwitchUsername;

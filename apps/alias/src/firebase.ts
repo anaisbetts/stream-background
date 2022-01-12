5e7f6b58cd6d10f8ff501ad2970e03436a897e01
Anaïs Betts <anais@anaisbetts.org>
2022-01-12 12:24:47 +0100

    Add secrets repo

--- apps/alias/src/firebase.ts
+++ apps/alias/src/firebase.ts
@@ -1,12 +1,12 @@
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
+import * as firebase from "firebase/app";
+import "firebase/firestore";
 
-import { firebaseConfig } from './firebase-config';
+import { firebaseConfig } from "secrets/firebase-config";
 
 try {
   firebase.initializeApp(firebaseConfig);
   // tslint:disable-next-line:no-empty
-} catch (_e) { }
+} catch (_e) {}
 
 export const db = firebase.firestore!();
 db.settings({});
--- apps/background/components/firebase.ts
+++ apps/background/components/firebase.ts
@@ -1,13 +1,13 @@
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
-import { firebaseConfig } from './firebase-config';
+import { firebaseConfig } from 'secrets/firebase-config'
 
 try {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
   // tslint:disable-next-line:no-empty
 } catch (_e) {}
 
-export const db = firebase.firestore!();
-db.settings({});
+export const db = firebase.firestore!()
+db.settings({})

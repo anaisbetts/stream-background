ef6ccc1057894a91b9619395a908d547428ebbb0
Anaïs Betts <anais@anaisbetts.org>
2022-01-31 21:53:04 -0500

    Set up testing

--- packages/config/eslint.js
+++ packages/config/eslint.js
@@ -1,11 +1,19 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  plugins: ['testing-library'],
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ['apps/*/', 'packages/*/'],
     },
   },
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    '@next/next/no-html-link-for-pages': 'off',
   },
-};
+}

67654f1d5a152778be467dcf0fd2e8e643426863
Anaïs Betts <anais@anaisbetts.org>
2022-02-06 15:28:42 -0500

    yes

--- packages/actions/src/utility.ts
+++ packages/actions/src/utility.ts
@@ -1,22 +1,36 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export function useMounted() {
-  const mounted = useRef<boolean>(true);
+  const mounted = useRef<boolean>(true)
 
   useEffect(() => {
-    mounted.current = true;
+    mounted.current = true
 
     return () => {
-      mounted.current = false;
-    };
-  }, [mounted]);
+      mounted.current = false
+    }
+  }, [mounted])
 
-  return mounted;
+  return mounted
 }
 
 export function useExplicitRender() {
-  const [n, setN] = useState(0);
+  const [n, setN] = useState(0)
 
-  const rerender = useCallback(() => setN((x) => x + 1), []);
-  return { dep: n, rerender };
+  const rerender = useCallback(() => setN((x) => x + 1), [])
+  return { dep: n, rerender }
+}
+
+export function cx(...args: (string | null | undefined | false)[]) {
+  return args
+    .reduce((acc: string[], arg) => {
+      const toSplit = arg || ''
+
+      toSplit.split(' ').forEach((x) => {
+        if (x && x.length > 1) acc.push(x)
+      })
+
+      return acc
+    }, [])
+    .join(' ')
 }
